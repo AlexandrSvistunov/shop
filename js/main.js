11d233161a4e51c21483d8f5c1071f442752b608
Alexandr Svistunov <alsvis2019@gmail.com>
2022-04-03 17:55:58 +0500

    Add cart.html

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
 
   let menuButton = document.querySelector(".header__burger");
   let menuClose = document.querySelector(".header__nav-close");
+  let cartClear = document.getElementById("clear");
   let menuNav = document.querySelector(".header__nav");
+  let cartItem = document.querySelector(".cart__item"); 
   // //Кликаем на бургер
   // menuButton.addEventListener("click", function () {
   //   menuNav.classList.toggle("header__nav--active");
@@ -18,4 +20,17 @@ $(document).ready(function () {
   menuClose.addEventListener("click", function () {
     $(menuNav).removeClass("header__nav--active");
   });
+
+  // Очищаем корзину
+  cartClear.addEventListener("click", function () {
+    $(".cart__form-group").addClass("cart__item--clear");
+  });
+
+
+  // Закрываем карточку
+
+  $('.cart__item-close').on("click", function () {
+    $(this).parents('fieldset').addClass("cart__item--clear");
+    });
+
 });
