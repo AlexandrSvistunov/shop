0c0ecee248e55c98a264beb5bbde266a7175b275
Alexandr Svistunov <alsvis2019@gmail.com>
2022-04-11 16:03:29 +0500

    Catalog.html add styles and JS

--- js/main.js
+++ js/main.js
@@ -3,34 +3,60 @@ $(document).ready(function () {
 
   let menuButton = document.querySelector(".header__burger");
   let menuClose = document.querySelector(".header__nav-close");
-  let cartClear = document.getElementById("clear");
   let menuNav = document.querySelector(".header__nav");
-  let cartItem = document.querySelector(".cart__item"); 
-  // //Кликаем на бургер
+
+   // //Кликаем на бургер
   // menuButton.addEventListener("click", function () {
   //   menuNav.classList.toggle("header__nav--active");
   // });
 
   // Наводим на бургер
   $(menuButton).hover(function() {
-    $(menuNav).addClass("header__nav--active");
+    $(menuNav).addClass('header__nav--active');
   });
 
   // Кликаем на крестик
   menuClose.addEventListener("click", function () {
-    $(menuNav).removeClass("header__nav--active");
+    $(menuNav).removeClass('header__nav--active');
   });
 
   // Очищаем корзину
-  cartClear.addEventListener("click", function () {
-    $(".cart__form-group").addClass("cart__item--clear");
-  });
-
+  $('#clear').on("click", function () {
+    $('.cart__form-group').addClass('cart__item--clear');
+    });
 
   // Закрываем карточку
 
   $('.cart__item-close').on("click", function () {
-    $(this).parents('fieldset').addClass("cart__item--clear");
+    $(this).parents('fieldset').addClass('cart__item--clear');
     });
 
+
+  // Открываем фильтр
+  $('.products__header-title').on("click", function () {
+    $(".filter").addClass('filter--active');
+    });
+
+  // Закрываем фильтр
+  $('.filter__title').on("click", function () {
+    $(".filter").removeClass('filter--active');
+    });
+
+
+  // Подменю фильтра
+  $('.filter__list-title').on("click", function () {
+    $(this).siblings().toggle('filter__submenu--active');
+  });
+
+  // Меню сортировки открываем
+  $('.products__sort-item').on("click", function () {
+    $(this).children('.products__sort-submenu').toggle('products__sort-submenu--active');
+  });
+
+  $('.products__sort-submenu').click(function (e) {
+    e.stopPropagation(); // Отменяем всплытие события от вслывающего меню
+  });
+
 });
+
+
